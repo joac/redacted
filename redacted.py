2b3602557654f2d041daadd961b953796fac3a6e
Joaquín Sorianello <joac@joac.com.ar>
2016-03-08 09:01:13 -0300

    Some API ideas

---
+++ redacted.py
@@ -0,0 +1,25 @@
+EXCEPTION_ON_LEAK = False  # Instead of printing, raise an exception on insecure access to data
+TRACE_SECRET_ACCESS = False  # Log stack traces of secure access to secret
+REDACTED_STR = "<REDACTED>"
+
+
+class Redacted(object):
+    """Encapsulates binary sensitive data"""
+
+
+    def __repr__(self):
+        return REDACTED_STR
+
+    def __format__(self, *args, **kwargs):
+        raise ValueError("Redacted Objects can't be unsafe formatted")
+
+    def __str__(self):
+        return REDACTED_STR
+
+
+class SecureFormatter(object):
+    """Provides pythonesque string formating, returning a Redacted"""
+
+
+class SecureSink(object):
+    """Provides a way to access stored secret"""
